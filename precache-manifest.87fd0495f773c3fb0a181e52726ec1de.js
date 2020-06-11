b8c5338d0740f85880b5bacd2e158bb159d86438
Reynaldo Choque <reynaldo.choque.apaza@gmail.com>
2020-06-10 21:49:00 -0400

    Updates

---
+++ precache-manifest.87fd0495f773c3fb0a181e52726ec1de.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a8b75103ab64134a8108f03f3aae50c7",
+    "url": "/covid19-live-stats/index.html"
+  },
+  {
+    "revision": "4e0e1406ba8e221f4c6f",
+    "url": "/covid19-live-stats/static/css/main.77c649e5.chunk.css"
+  },
+  {
+    "revision": "347cd66902b928b4f4e7",
+    "url": "/covid19-live-stats/static/js/2.56dacabe.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/covid19-live-stats/static/js/2.56dacabe.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4e0e1406ba8e221f4c6f",
+    "url": "/covid19-live-stats/static/js/main.586e6661.chunk.js"
+  },
+  {
+    "revision": "d1a87941c395b3e90f8f",
+    "url": "/covid19-live-stats/static/js/runtime-main.3cae97e5.js"
+  }
+]);
